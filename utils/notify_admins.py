5b34f03a22d2f579f228593594130f3a238a9220
AkrombekAmirov <akrombekamirov@gmail.com>
2025-04-21 17:28:32 +0500

    17:28 time 21.04.2025 date

--- utils/notify_admins.py
+++ utils/notify_admins.py
@@ -2,12 +2,12 @@ import logging
 
 from aiogram import Dispatcher
 
-from data.config import ADMIN_M2
+from data.config import ADMIN_M1
 
 
 async def on_startup_notify(dp: Dispatcher):
     try:
-        await dp.bot.send_message(ADMIN_M2, "Bot ishga tushdi. /start")
+        await dp.bot.send_message(ADMIN_M1, "Bot ishga tushdi. /start")
 
     except Exception as err:
         logging.exception(err)
