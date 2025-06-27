f437e5ec0c0245494f703a3b798f410f8d290420
AkrombekAmirov <akrombekamirov@gmail.com>
2025-06-27 10:35:08 +0500

    10:34 time 27.06.2025 date

--- file_service/hisobot/__init__.py
+++
@@ -1,3 +0,0 @@
-from .get_report_file import get_report_file_path
-
-__all__ = ['get_report_file_path']
--- file_service/hisobot/get_report_file.py
+++
@@ -1,5 +0,0 @@
-from os.path import join, dirname
-
-
-async def get_report_file_path(name: str):
-    return join(dirname(__file__), name)
